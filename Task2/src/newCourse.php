a83228bf713401ee6e84bb959ea96cee533da2cf
Eden Brawn <edenbrawn@gmail.com>
2023-05-29 20:15:23 +0100

    added a new course form

--- Task2/src/newCourse.php
+++ Task2/src/newCourse.php
@@ -25,6 +25,18 @@
                     <img src="./sampleEntryForm.png" alt="New course entry form">
                 </div>    
                 <div class="addmore">
+                    <h2>Here you can add an article:</h2>
+                    <form action="newCourse.php" method="POST">
+                        <label>Enter Course Details</label>
+                        <label>Course Title</label> <input type="text" name="course_title"/>
+                        <label>UCAS Code</label> <input type="text" name="course_ucas_code"/>
+                        <label>Course Level</label> <input type="text" name="course_level"/>
+                        
+                        <input type="submit" name="newcourse" value="Submit" />
+                    </form>
+
+
+
                     <!-- add more feilds for the remaining recipe info ...-->
                     <p class="note">The sketch above provides an incomplete list of the required information for a new course. You need to add the missing feilds to the New Course Entry Form you are going to develop.</p>
                     <input type="submit" value="Add Course" />
