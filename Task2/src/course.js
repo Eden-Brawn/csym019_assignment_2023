a85345fa528314d4ae552f0c60cd3eea7a355e2a
Eden Brawn <edenbrawn@gmail.com>
2023-05-30 00:23:11 +0100

    js to show different sections based on buttons

--- Task2/src/course.js
+++ Task2/src/course.js
@@ -7,13 +7,28 @@ function addEditDelete() {
     add.addEventListener('click', del);
 }
 function add(){
-
+    let removeEdit = document.getElementsByClassName('editcourse');
+    removeEdit.style.display = 'none';
+    let removeDelete = document.getElementsByClassName('delcourse');
+    removeDelete.style.display = 'none';
+    let showAdd = document.getElementsByClassName('addcourse');
+    showAdd.style.display = 'block';
 }
 function edit(){
-
+    let showEdit = document.getElementsByClassName('editcourse');
+    showEdit.style.display = 'block';
+    let removeDelete = document.getElementsByClassName('delcourse');
+    removeDelete.style.display = 'none';
+    let removeAdd = document.getElementsByClassName('addcourse');
+    removeAdd.style.display = 'none';
 }
 function del(){
-
+    let removeEdit = document.getElementsByClassName('editcourse');
+    removeEdit.style.display = 'none';
+    let showDelete = document.getElementsByClassName('delcourse');
+    showDelete.style.display = 'show';
+    let removeAdd = document.getElementsByClassName('addcourse');
+    removeAdd.style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', addEditDelete);
