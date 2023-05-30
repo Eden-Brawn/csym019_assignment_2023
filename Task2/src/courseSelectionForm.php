db90ccea75c92f0d4d6e7f14eabb218a561d0f94
Eden Brawn <edenbrawn@gmail.com>
2023-05-30 19:45:35 +0100

    added all the data to the table on course report

--- Task2/src/courseSelectionForm.php
+++ Task2/src/courseSelectionForm.php
@@ -24,13 +24,69 @@
             <?php
             $course = $pdo->prepare('SELECT * FROM course');
             $course ->execute();
-                echo '<table>';
-                echo '<tr><th>Course Name</th><th>UCAS Code</th><th>Level</th></tr>';
+                echo '<table>'.
+                '<tr><th>Course Name</th><th>UCAS Code</th><th>Level</th></tr>';
                 foreach ($course as $row){  
                     echo '<tr>' .
                             '<td>' .$row['course_title'] . '</td>'.
                             '<td>'. $row['course_ucas_code'] . '</td>'.
                             '<td>' . $row['course_level'] . '</td>'.
+                            '<td>' .$row['course_start'] . '</td>'.
+                            '<td>'. $row['location'] . '</td>'.
+                            '<td>' . $row['course_description'] . '</td>'.
+                            '<td>' .$row['course_satisfaction'] . '</td>'.
+                            '<td>'. $row['duration_fulltime'] . '</td>'.
+                            '<td>' . $row['duration_parttime'] . '</td>'.
+                            '<td>' .$row['duration_foundation'] . '</td>'.
+                            '<td>'. $row['uk_fees_fulltime'] . '</td>'.
+                            '<td>' . $row['uk_fees_parttime'] . '</td>'.
+                            '<td>' .$row['uk_fees_foundation'] . '</td>'.
+                            '<td>'. $row['international_fees_fulltime'] . '</td>'.
+                            '<td>' . $row['international_fees_foundation'] . '</td>'.
+                            '<td>' .$row['work_placement'] . '</td>'.
+                            '<td>'. $row['entry_grades_alevel'] . '</td>'.
+                            '<td>' . $row['entry_grades_btec'] . '</td>'.
+                            '<td>' .$row['entry_grades_tlevel'] . '</td>'.
+                            '<td>'. $row['foundation_grades_alevel'] . '</td>'.
+                            '<td>' . $row['foundation_grades_btec'] . '</td>'.
+                            '<td>' .$row['foundation_grades_tlevel'] . '</td>'.
+                            '<td>'. $row['language_requirements'] . '</td>'.
+                            '<td>' . $row['year1_module1'] . '</td>'.
+                            '<td>' .$row['y1_m1_credits'] . '</td>'.
+                            '<td>'. $row['year1_module2'] . '</td>'.
+                            '<td>' . $row['y1_m2_credits'] . '</td>'.
+                            '<td>' .$row['year1_module3'] . '</td>'.
+                            '<td>'. $row['y1_m3_credits'] . '</td>'.
+                            '<td>' . $row['year1_module4'] . '</td>'.
+                            '<td>' .$row['y1_m4_credits'] . '</td>'.
+                            '<td>'. $row['year1_module5'] . '</td>'.
+                            '<td>' . $row['y1_m5_credits'] . '</td>'.
+                            '<td>' .$row['year1_module6'] . '</td>'.
+                            '<td>'. $row['y1_m6_credits'] . '</td>'.
+                            '<td>' . $row['year2_module1'] . '</td>'.
+                            '<td>' .$row['y2_m1_credits'] . '</td>'.
+                            '<td>'. $row['year2_module2'] . '</td>'.
+                            '<td>' . $row['y2_m2_credits'] . '</td>'.
+                            '<td>' .$row['year2_module3'] . '</td>'.
+                            '<td>'. $row['y2_m3_credits'] . '</td>'.
+                            '<td>' . $row['year2_module4'] . '</td>'.
+                            '<td>' .$row['y2_m4_credits'] . '</td>'.
+                            '<td>'. $row['year2_module5'] . '</td>'.
+                            '<td>' . $row['y2_m5_credits'] . '</td>'.
+                            '<td>' .$row['year2_module6'] . '</td>'.
+                            '<td>'. $row['y2_m6_credits'] . '</td>'.
+                            '<td>' . $row['year3_module1'] . '</td>'.
+                            '<td>' .$row['y3_m1_credits'] . '</td>'.
+                            '<td>'. $row['year3_module2'] . '</td>'.
+                            '<td>' . $row['y3_m2_credits'] . '</td>'.
+                            '<td>' .$row['year3_module3'] . '</td>'.
+                            '<td>'. $row['y3_m3_credits'] . '</td>'.
+                            '<td>' . $row['year3_module4'] . '</td>'.
+                            '<td>' .$row['y3_m4_credits'] . '</td>'.
+                            '<td>'. $row['year3_module5'] . '</td>'.
+                            '<td>' . $row['y3_m5_credits'] . '</td>'.
+                            '<td>' .$row['year3_module6'] . '</td>'.
+                            '<td>'. $row['y3_m6_credits'] . '</td>'.
                         '</tr>';
                 }
                 echo '</table>';
