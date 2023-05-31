ae9f60912324d3804523522ea36e63d725c7d485
Eden Brawn <edenbrawn@gmail.com>
2023-05-31 20:35:43 +0100

    linked to the ciurse report page

--- Task2/src/courseSelectionForm.php
+++ Task2/src/courseSelectionForm.php
@@ -20,7 +20,7 @@
         </nav>
         <main>
             <h3>Sample Course Selection Form</h3>
-            <form action="./sampleReport.png" >
+            <form action="sampleCourseReport.php" >
                 <div class="container">    
                 <?php
                 $course = $pdo->prepare('SELECT * FROM course');
