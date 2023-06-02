db4128bbc683646414f4bf7e43404ee7bbcd769c
Eden Brawn <edenbrawn@gmail.com>
2023-06-02 14:17:58 +0100

    added a signin page

---
+++ Task2/src/signinsignup.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Course Report</title>
+        <link rel="stylesheet" href="layout.css">
+        <script src="course.js"></script>
+    </head>
+    <body>
+        <header>
+            <h3>CSYM019 - UNIVERSITY COURSES</h3>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="./courseSelectionForm.php">Course Report</a></li>
+                <li><a href="./newCourse.php">New Course</a></li>
+            </ul>
+        </nav>
+        <main>
+
+        </main>
+        <footer>&copy; CSYM019 2023</footer>
+    </body>
+</html>
