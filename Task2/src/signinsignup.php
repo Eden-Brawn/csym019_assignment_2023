9acddc9c3745ea37269570b00f3fe15b2809879f
Eden Brawn <edenbrawn@gmail.com>
2023-06-02 14:38:35 +0100

    added passwords to database php

--- Task2/src/signinsignup.php
+++ Task2/src/signinsignup.php
@@ -1,6 +1,36 @@
 <?php
     require 'mySql.php';
-
+    if(isset($_POST['signin'])){/*sign in to the website*/
+        $search = $pdo->prepare('SELECT * FROM admins WHERE admin_name = :adminname');
+        $values = [
+            'adminname' => $_POST['admin']
+        ];
+        $search->execute($values);
+        $entry = $search->fetch();
+        if (password_verify($_POST['pass'], $entry['admin_password'])) {/*As the password is hashed we will have to use a different method to compare them*/
+            $_SESSION['logged'] = true; /*set the session*/
+            //$_SESSION['user'] = $_POST['usersname'];
+            header("location:newCourse.php");
+        }
+        else{
+            echo 'Not Recognised, Retry Please.';
+        }
+    }
+    
+    
+    if(isset($_POST['userssignup'])){
+        $add = $pdo->prepare('INSERT INTO admins (admin_name, admin_password) /*sign up page so insert it into the database*/
+                                        VALUES (:adminnew, :passnew)');
+            $_SESSION['logged'] = true; /*sets sessions*/
+            //$_SESSION['user'] = $_POST['usersname'];
+            $passwordhash = password_hash($_POST['passnew'], PASSWORD_DEFAULT);/*use the taught hash to encrypt the password*/
+            $new = [
+                'adminnew' => $_POST['adminnew'],
+                'passnew' => $passwordhash
+            ];
+            $add->execute($new); 
+            header("location:newCourse.php");
+        } 
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,15 +51,15 @@
         <main>
             <h2>Please Sign IN:</h2>
             <form action="signinsignup.php" method="POST">
-                <label>Admin name</label> <input type="text" name="usersname"/>
-                <label>Password</label> <input type="text" name="userpassword"/>
+                <label>Admin name</label> <input type="text" name="admin"/>
+                <label>Password</label> <input type="text" name="pass"/>
 
                 <input type="submit" name="signin" value="Submit" />
             </form>
             <h2>Please Sign Up:</h2>
             <form action="signinsignup.php" method="POST">
-                <label>Admin name</label> <input type="text" name="usersname"/>
-                <label>Password</label> <input type="text" name="userpassword"/>
+                <label>Admin name</label> <input type="text" name="adminnew"/>
+                <label>Password</label> <input type="text" name="passnew"/>
 
                 <input type="submit" name="signup" value="Submit" />
             </form>
